be80a6902348d9cc641be9bea074c2e3d8f4a4bc
unknown <zaman-pro>
2025-07-26 01:11:17 +0600

    setup tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,18 @@ import { RouterProvider } from "react-router";
 import { router } from "./routes/Routes.jsx";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "./providers/AuthProvider.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// Create a client
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <AuthProvider>
-      <RouterProvider router={router} />
-      <Toaster />
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </AuthProvider>
+    </QueryClientProvider>
   </StrictMode>
 );
--- src/pages/Dashboard/DashboardHome.jsx
+++ src/pages/Dashboard/DashboardHome.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import useAuth from "../../hooks/useAuth";
 
 const DashboardHome = () => {
+  const { user } = useAuth();
+
   return (
-    <div>
-      <h2>This is dashboard Home</h2>
+    <div className="p-6 space-y-6">
+      {/* Welcome Section */}
+      <div className="flex items-center space-x-4">
+        <img
+          src={user?.photoURL}
+          alt="User Avatar"
+          className="w-16 h-16 rounded-full ring-2 ring-blood"
+        />
+        <h1 className="text-2xl font-semibold">
+          Welcome, {user?.displayName}!
+        </h1>
+      </div>
     </div>
   );
 };
