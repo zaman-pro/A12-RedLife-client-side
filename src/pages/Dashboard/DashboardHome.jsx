183eda5a93051187fcb56fc36137260c1ce62f95
unknown <zaman-pro>
2025-07-25 22:31:25 +0600

    add dashboard route

---
+++ src/pages/Dashboard/DashboardHome.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DashboardHome = () => {
+  return (
+    <div>
+      <h2>This is dashboard Home</h2>
+    </div>
+  );
+};
+
+export default DashboardHome;
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter } from "react-router";
 import Home from "../pages/Home/Home";
-import Root from "../layouts/MainLayout";
+import Root from "../layouts/Root";
 import Auth from "../pages/Auth/Auth";
+import DashboardLayout from "../layouts/DashboardLayout";
+import DashboardHome from "../pages/Dashboard/DashboardHome";
 
 export const router = createBrowserRouter([
   {
@@ -9,12 +11,22 @@ export const router = createBrowserRouter([
     element: <Root />,
     children: [
       {
-        path: "/",
+        index: true,
         element: <Home />,
       },
       {
         path: "/auth",
         Component: Auth,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <DashboardHome />,
       },
     ],
   },
