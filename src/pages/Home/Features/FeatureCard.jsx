38161ed0ea1fd6f68b66bf55ea0db5d9d5b44b01
unknown <zaman-pro>
2025-08-17 20:00:19 +0600

    rearrange feature card

--- src/pages/Home/Features/FeatureCard.jsx
+++ src/pages/Home/Features/FeatureCard.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const FeatureCard = ({ icon: Icon, title, description }) => {
   return (
-    <div className="relative group card bg-base-200 shadow-xl hover:shadow-lg hover:shadow-accent px-6 py-8 flex flex-col items-center text-center transition-shadow duration-300">
-      <div className="text-5xl mb-4 transition-transform duration-300 ease-in-out group-hover:scale-110">
+    <div className="relative flex flex-col items-center text-center bg-base-100 border border-secondary/20 rounded-lg p-6">
+      <div className="text-3xl mb-4">
         <Icon />
       </div>
-      <h3 className="text-2xl font-semibold">{title}</h3>
+      <h3 className="text-xl font-semibold">{title}</h3>
 
       {/* Truncated description */}
       <p className="mt-2 line-clamp-2 relative z-10">{description}</p>
