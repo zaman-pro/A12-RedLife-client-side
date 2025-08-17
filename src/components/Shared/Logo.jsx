796c98c7fcceaf7cbfe9218ed216c218663b75f2
unknown <zaman-pro>
2025-08-17 22:20:39 +0600

    replace logo and fav icon with svg

--- src/components/Shared/Logo.jsx
+++ src/components/Shared/Logo.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import { Link } from "react-router";
+import fav from "/fav.svg";
 
 const Logo = () => {
   return (
     <Link to="/" className="flex items-center">
-      <img
-        className="w-10 mr-2 hidden md:flex"
-        src="https://i.ibb.co/wrY5MYL0/red-life-icon.png"
-        alt="Logo"
-      />
-      <span className="hidden lg:flex text-3xl font-bold text-secondary">
+      <img className="w-8 mr-1 hidden md:flex" src={fav} alt="Logo" />
+      <span className="hidden lg:flex text-3xl font-bold text-secondary leading-8">
         RedLife
       </span>
     </Link>
